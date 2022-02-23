9df12ec0b9e857f98f0779af32cd32a57defdf75
Derek <derekwellis@outlook.com>
2022-02-23 14:15:42 -0600

    Initial commit

---
+++ src/com/derek/Main.java
@@ -0,0 +1,25 @@
+package com.derek;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("upper class code");
+
+        try {
+            System.out.println("unexceptional code");
+            System.out.println("exceptional code is " + 100/0);
+        } catch (ArithmeticException e) {
+            System.out.println("arithmetic exception");
+        } catch (Exception e) {
+            System.out.println("generic exception");
+        } finally {
+            System.out.println("finally done");
+        }
+
+        System.out.println("bottom class code");
+
+    }
+}
+
